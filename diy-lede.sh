968ac2ff7d20f583e60187656570cbda84c10f31
johnboypark <104748386+johnboypark@users.noreply.github.com>
2023-07-18 21:34:42 +0800

    Update diy-lede.sh

--- diy-lede.sh
+++ diy-lede.sh
@@ -8,7 +8,7 @@ rm -rf package/lean/luci-theme-argon
 #
 
 # 修改openwrt登陆地址,把下面的192.168.123.1修改成你想要的就可以了，其他的不要动
-sed -i 's/192.168.1.1/192.168.123.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.2.10/g' package/base-files/files/bin/config_generate
 
 
 #修改版本内核（下面两行代码前面有#为源码默认最新5.4内核,没#为4.19内核,默认修改X86的，其他机型L大那里target/linux查看，对应修改下面的路径就好）
@@ -19,11 +19,11 @@ sed -i 's/192.168.1.1/192.168.123.1/g' package/base-files/files/bin/config_gener
 #取消掉feeds.conf.default文件里面的helloworld的#注释
 sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default  #使用源码自带ShadowSocksR Plus+出国软件
 
+
 # Add a feed source增加默认源地址
 sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
-
-
-
+sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
 sed -i 's@.*CYXluq4wUazHjmCDBCqXF*@#&@g' package/lean/default-settings/files/zzz-default-settings  #设置密码为空
 
 
